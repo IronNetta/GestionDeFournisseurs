7ae866985e4b0f41996924523b7c288cc9d48ee0
Sebastien De Laet <delaet.sebastien93@gmail.com>
2024-11-08 09:48:12 +0100

    ajout javadoc

--- src/Fournisseur.java
+++ src/Fournisseur.java
@@ -13,9 +13,16 @@ public class Fournisseur {
         this.commandeEnCours = commandeEnCours;
     }
 
+    /**
+     * Retourne une chaine de caracteres decrivant le fournisseur.
+     * La chaine contient l'identifiant, le nom, le numero de telephone,
+     * l'adresse e-mail et le statut de la commande en cours.
+     * 
+     * @return une cha ne de caracteres decrivant le fournisseur
+     */
     @Override
     public String toString() {
-        return "ID: " + id + ", Nom: " + nom + ", Téléphone: " + telephone + ", Email: " + email + ", Commande en cours: " + commandeEnCours;
+        return "ID: " + id + ", Nom: " + nom + ", Téléphone: " + telephone + ", Email: " + email
+                + ", Commande en cours: " + commandeEnCours;
     }
 }
-
